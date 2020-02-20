e60498409f59c33ca185f96adc49dc879885855c
Wallerand Lucas <lucas.wallerand.etu@univ-lille.fr>
2020-02-20 19:47:18 +0100

    feat: score computing function

--- ptdr.py
+++ ptdr.py
@@ -1,3 +1,5 @@
+import math
+
 fd = open("a_example.txt",'r')
 
 line = fd.readline().split(' ')
@@ -7,8 +9,17 @@ D = int(line[2]) #number of days
 
 line = fd.readline().split(' ')
 S = [int(e) for e in line]
-
 super = []
+
+def compute_score(library, score_list):
+    """
+    A function for computing the library score
+    """
+    total_score = 0
+    for book in library["books"]:
+        total_score += S[book]
+    return total_score / (library["signup"] + math.ceil(library["nbooks"] / library["bpd"]))
+
 
 for i in range(L):
     #library
@@ -16,10 +27,11 @@ for i in range(L):
     line = fd.readline().split(' ')
     super[i]["nbooks"] = int(line[0]) #number of books in library
     super[i]["signup"] = int(line[1]) #number of days to finish signup
-    super[i]["bpd"] = int(line[2]) #number of books/day after sign up
+    super[i]["bpd"] = int(line[2]) #number of books/day after sign up    
     line = fd.readline().split(' ')
     assert(len(line)==super[i]["nbooks"])
     super[i]["books"] = [int(e) for e in line]
+    super[i]["score"] = compute_score(super[i] ,S)
 
 fd.close()
 
